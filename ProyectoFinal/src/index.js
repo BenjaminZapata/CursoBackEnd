8e1ad6c9bb37ae3a7ae48ab89c33e6a9f57aa9dd
Benjamin Zapata <benjaminzapata1998@gmail.com>
2023-04-20 11:35:23 -0300

    20/04 11:35

--- ProyectoFinal/src/index.js
+++ ProyectoFinal/src/index.js
@@ -1,14 +1,41 @@
 const express = require('express');
+const { Server } = require('socket.io')
+const ProductManager = require('./ProductManager.js')
+const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const productsRouter = require("./routes/products.router.js")
 const cartsRouter = require("./routes/carts.router.js")
+const productsList = require("./routes/productsList.router.js")
+const realTimeProducts = require("./routes/realTimeProducts.router.js")
 
+const productManager = new ProductManager()
 const app = express()
+const httpServer = app.listen(8080, () => console.log("Servidor on"))
+const socketServer = new Server(httpServer)
+
+app.engine('handlebars', handlebars.engine())
 app.use(bodyParser.json())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname+'/public'))
+app.set('views', __dirname+'/views')
+app.set('view engine', 'handlebars')
 
+app.use('/home', productsList)
+app.use('/realtimeproducts', realTimeProducts)
 app.use('/products', productsRouter)
 app.use('/carts', cartsRouter)
 
-app.listen(8080, () => console.log('Server up'))
+
+socketServer.on('connection', (socketClient) =>{
+  let productsWebsocket = productManager.getProducts()
+  socketClient.emit("products", productsWebsocket)
+  socketClient.on("product", data => {
+    productManager.addProduct(data.title, data.description, data.price, data.thumbnails, data.code, data.stock, true, data.category)
+    socketClient.emit("products", productManager.getProducts())
+  })
+  socketClient.on("erase", data => {
+    productManager.deleteProduct(data)
+    socketClient.emit("products", productManager.getProducts())
+  })
+})
